d1f8bb2795324e3585338b758b1c07061723d1aa
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-01 19:42:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import altair as alt
-import random
-import numpy as np
+#import random
+#import numpy as np
 
 st.set_page_config(page_title=" Панель текущих продаж менеджеров", page_icon=":bar_chart:", layout="wide")
 
