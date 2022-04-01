1332839d74df0e2e7e4edaf0a45fcfe8c873e8ec
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-01 21:13:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ df = get_data_from_excel()
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
-manager = st.sidebar.radio(
+manager = st.sidebar.selectbox(
     "Выбери менеджера:",
     options=df['Менеджер'].unique(),
     
