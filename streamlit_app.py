258f8acd4e899d65e1c41e9ff55496e0d887b625
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-02 23:27:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ st.markdown("###")
 st.table(t11.style.highlight_max(color='yellowgreen', subset='Потребление:Тонны'))
 st.table(t11.style.highlight_max(color='yellowgreen', subset='Потребление:Тонны'
                                 ).format("{:.1f}").background_gradient(cmap='Blues', axis=1)
-         
+        )
 st.dataframe(t11.style.highlight_max(axis=0))
          
 st.markdown("""---""")
