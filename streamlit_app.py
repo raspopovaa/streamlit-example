d003cfd1bf1773ea9007e1255ba27ecfc246b4b6
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-03 00:13:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ t12['Тонны'] = t12['Тонны'].astype('int')
 
 # ---- MAINPAGE ----
 st.title(":bar_chart: Показатели активности клиентов")
-st.markdown("## Основные метрики")
+st.markdown("### Основные метрики")
 total_sales = int(df_selection['Тонны'].sum())
 average_rating = round(df_selection.groupby('Наименование_клиента')['Тонны'].mean().mean(), 1)
 average_sale_by_transaction = round(df_selection['Наименование_клиента'].nunique(), 0)
@@ -83,7 +83,6 @@ col2.metric("Среднее потребление клиента: тонн", av
 col3.metric("Количество активных клиентов: клиентов", average_sale_by_transaction,)
 
 st.markdown("""---""")
-st.header(":articulated_lorry: ТОП-10 клиентов")
 st.markdown("### :articulated_lorry: ТОП-10 клиентов")
 
 st.table(t11.style.background_gradient(axis=0, gmap=t11['Потребление:Тонны'], cmap='Blues'))
