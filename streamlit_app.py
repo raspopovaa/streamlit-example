46414ef29df4c7b5fe945f9b41dde2d7a0f439e3
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-02 23:31:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ import altair as alt
 #import random
 import openpyxl
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title=" Панель текущих продаж менеджеров", page_icon=":bar_chart:", layout="wide")
 
