ff739850701f7f6cb7d0c6bb9fd7b6dda0d145cf
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-03 01:11:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,7 @@ st.altair_chart(c, use_container_width=True)
 
 t01 = df_selection.groupby(['Наименование_клиента'],as_index=False)['Тонны'].sum().nlargest(5,'Тонны')
 t02 = t01['Наименование_клиента']
-t13 = df.query('Наименование_клиента in @t02').groupby(['month','Наименование_клиента',],as_index=False)['Тонны'].sum()
+t13 = df_selection.query('Наименование_клиента in @t02').groupby(['month','Наименование_клиента',],as_index=False)['Тонны'].sum()
 
 d = alt.Chart(t13).mark_trail().encode(
    x="month",
