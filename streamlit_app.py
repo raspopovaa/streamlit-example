7e87d4155152ca4e7f5f5be31aa3da2d87030e35
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-04 22:36:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ df_selection = df.query(
     'Менеджер in @manager & month in @month & Сегмент in @segment & НГ in @prod & Отделение in @otdel'
 )
 
-t10 = df_selection.groupby(['Наименование_клиента'],as_index=False)['Тонны'].agg(['sum', 'mean','count'])
+t10 = df_selection.groupby(['Наименование_клиента'],as_index=False)['Тонны'].agg(['sum', 'mean','count']).reset_index()
 t10.index = np.arange(1, len(t10)+1)
 top_10 = t10.rename(columns={'sum':'Общее потребление', 'mean':'Среднее потребление', 'count':'Количество заправок'})
 # ---- MAINPAGE ----
