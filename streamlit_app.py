b484586206e622cb8d900c5ec6a78481bb9f04e0
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-06 21:50:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,35 +140,6 @@ def get_data_from_par():
 df2 = get_data_from_par()
 st.table(df2.head(5))
 
-st.pydeck_chart(pdk.Deck(
-     map_style='mapbox://styles/mapbox/light-v9',
-     initial_view_state=pdk.ViewState(
-         latitude=37.76,
-         longitude=-122.4,
-         zoom=11,
-         pitch=50,
-     ),
-     layers=[
-         pdk.Layer(
-            'HexagonLayer',
-            data=df,
-            get_position='[lon, lat]',
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            pickable=True,
-            extruded=True,
-         ),
-         pdk.Layer(
-             'ScatterplotLayer',
-             data=df2,
-             get_position='[lon, lat]',
-             get_color='[200, 30, 0, 160]',
-             get_radius=200,
-         ),
-     ],
- ))
-
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
