0222e0c86febfad20d3b21984e828b4cd25b7dff
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-06 22:33:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,7 @@ def get_data_from_par():
 
 geo = get_data_from_par()
 geo = geo.rename(columns = {'lot':'lon'})
-geo[['lat','lon']] = float(geo[['lat','lon']])
+geo[['lat','lon']] = (geo[['lat','lon']]).astype('float')
 st.table(geo.head(3))
 st.map(geo)
 
