05f2e03b8de3503c00c5b76014465246d2ad5bd4
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-04-06 21:11:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,6 +125,56 @@ tt[(tt.columns[1])] = tt[(tt.columns[1])].apply(null)
 
 st.table(tt[tt[(tt.columns[1])] == 0])
 
+st.markdown("""---""")
+st.markdown("### :articulated_lorry:   Карта распределения заправок")
+
+# LOAD DATA ONCE
+@st.experimental_singleton
+def load_data():
+    data = pd.read_csv(
+        "uber-raw-data-sep14.csv.gz",
+        nrows=100000,  # approx. 10% of data
+        names=[
+            "date/time",
+            "lat",
+            "lon",
+        ],  # specify names directly since they don't change
+        skiprows=1,  # don't read header since names specified directly
+        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
+        parse_dates=[
+            "date/time"
+        ],  # set as datetime instead of converting after the fact
+    )
+
+    return data
+
+
+# FUNCTION FOR AIRPORT MAPS
+def map(data, lat, lon, zoom):
+    st.write(
+        pdk.Deck(
+            map_style="mapbox://styles/mapbox/light-v9",
+            initial_view_state={
+                "latitude": lat,
+                "longitude": lon,
+                "zoom": zoom,
+                "pitch": 50,
+            },
+            layers=[
+                pdk.Layer(
+                    "HexagonLayer",
+                    data=data,
+                    get_position=["lon", "lat"],
+                    radius=100,
+                    elevation_scale=4,
+                    elevation_range=[0, 1000],
+                    pickable=True,
+                    extruded=True,
+                ),
+            ],
+        )
+    )
+
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
