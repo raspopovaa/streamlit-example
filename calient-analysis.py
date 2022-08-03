69dbbfdc58b1cb7ffcfcc5600c2e7bdf59aba82f
Andrey Raspopov <82306471+raspopovaa@users.noreply.github.com>
2022-08-03 15:49:33 +0700

    Update calient-analysis.py

--- calient-analysis.py
+++ calient-analysis.py
@@ -84,8 +84,8 @@ col1, col2, col3, col4 = st.columns(4)
 
 col1.metric("Общая реализация: тонн", total_sales,)
 col2.metric("Среднее потребление клиента: тонн", average_rating, )
-col3.metric("Сумма сервисного сбора: рублей", ss,)
-col4.metric("Сумма скидки: рублей", sale,)
+col4.metric("Сумма сервисного сбора: рублей", ss,)
+col3.metric("Сумма скидки: рублей", sale,)
 
 st.markdown("""---""")
 st.markdown("### :articulated_lorry: ТОП-5 карт")
